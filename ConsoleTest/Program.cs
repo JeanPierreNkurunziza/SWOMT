caf2f1b90c667eb8f93db0b22b701b4ae1468830
NKURUNZIZA JEAN PIERRE <nkuru2001@yahoo.com>
2021-05-21 13:58:47 +0200

    Revert "Updates"
    This reverts commit 5ecaa3e805b86bce7b0fbb587ecf21489fd640d6.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -16,17 +16,17 @@ namespace ConsoleTest
             //    Console.WriteLine(user.UserName);
             //    Console.ReadLine();
             //}
-           // MyApps.Infrastructure.DB.Utilisateur utilisateur = new MyApps.Infrastructure.DB.Utilisateur();
-           // utilisateur.UserName = "Admin";
-           // utilisateur.MotDePasse = MyApps.Domain.SecuriteService.AuthentificationService.PasswordSecurity.CreateHash("Admin1010.");
-           // utilisateur.UserRole = "Admin";
-           // MyApps.Domain.Service.UserService.Ajouter(utilisateur);
-           //// var nom= MyApps.Domain.SecuriteService.AuthentificationService.PasswordSecurity.verifyCrypto("Admin","admin");
+            MyApps.Infrastructure.DB.Utilisateur utilisateur = new MyApps.Infrastructure.DB.Utilisateur();
+            utilisateur.UserName = "Admin";
+            utilisateur.MotDePasse = MyApps.Domain.SecuriteService.AuthentificationService.PasswordSecurity.CreateHash("Admin1010.");
+            utilisateur.UserRole = "Admin";
+            MyApps.Domain.Service.UserService.Ajouter(utilisateur);
+           // var nom= MyApps.Domain.SecuriteService.AuthentificationService.PasswordSecurity.verifyCrypto("Admin","admin");
 
-           // Console.WriteLine("l'ajout à été réalisé");
-           // //////var nom=  SWOMT.SWOMTAUTHENTIFICATION.PasswordStorage.verifCrypto("hinda","hinda");
-           // //////  Console.WriteLine(nom);
-           // Console.ReadLine(); 
+            Console.WriteLine("l'ajout à été réalisé");
+            //////var nom=  SWOMT.SWOMTAUTHENTIFICATION.PasswordStorage.verifCrypto("hinda","hinda");
+            //////  Console.WriteLine(nom);
+            Console.ReadLine(); 
         } 
     }
 }
